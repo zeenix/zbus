44faeeb54ac92117004aadcd33fa6e9126cfa7e7
Zeeshan Ali Khan <zeenix@gmail.com>
2024-01-25 15:20:50 +0200

    💥 zb: Split Guid into reference & owned types
    `Guid` now has a lifetime parameter, and `OwnedGuid` is a new type that's
    a wrapper around `Guid<'static>`. Internally, `Guid` is now a wrapper
    around `zvariant::Str` and is now a very similar type to other
    `zvariant::Str` wrapper types.
    
    This also turns `guid` argument of `connection::Builder::server` into a
    generic type.

--- zbus/src/address/mod.rs
+++ zbus/src/address/mod.rs
@@ -11,7 +11,7 @@
 
 pub mod transport;
 
-use crate::{Error, Guid, Result};
+use crate::{Error, Guid, OwnedGuid, Result};
 #[cfg(all(unix, not(target_os = "macos")))]
 use nix::unistd::Uid;
 use std::{collections::HashMap, env, str::FromStr};
@@ -25,7 +25,7 @@ pub use self::transport::Transport;
 #[derive(Clone, Debug, PartialEq, Eq)]
 #[non_exhaustive]
 pub struct Address {
-    guid: Option<Guid>,
+    guid: Option<OwnedGuid>,
     transport: Transport,
 }
 
@@ -39,10 +39,14 @@ impl Address {
     }
 
     /// Set the GUID for this address.
-    pub fn set_guid(mut self, guid: Guid) -> Self {
-        self.guid = Some(guid);
+    pub fn set_guid<G>(mut self, guid: G) -> Result<Self>
+    where
+        G: TryInto<OwnedGuid>,
+        G::Error: Into<crate::Error>,
+    {
+        self.guid = Some(guid.try_into().map_err(Into::into)?);
 
-        self
+        Ok(self)
     }
 
     /// The transport details for this address.
@@ -106,8 +110,8 @@ impl Address {
     }
 
     /// The GUID for this address, if known.
-    pub fn guid(&self) -> Option<&Guid> {
-        self.guid.as_ref()
+    pub fn guid(&self) -> Option<&Guid<'_>> {
+        self.guid.as_ref().map(|guid| guid.inner())
     }
 }
 
@@ -147,7 +151,10 @@ impl FromStr for Address {
         }
 
         Ok(Self {
-            guid: options.remove("guid").map(Guid::from_str).transpose()?,
+            guid: options
+                .remove("guid")
+                .map(|s| Guid::from_str(s).map(|guid| OwnedGuid::from(guid).to_owned()))
+                .transpose()?,
             transport: Transport::from_options(transport, options)?,
         })
     }
@@ -238,7 +245,8 @@ mod tests {
             Address::from(Transport::Unix(Unix::new(UnixPath::File(
                 "/tmp/dbus-foo".into()
             ))))
-            .set_guid(guid),
+            .set_guid(guid)
+            .unwrap(),
         );
         assert_eq!(
             Address::from_str("tcp:host=localhost,port=4142").unwrap(),
